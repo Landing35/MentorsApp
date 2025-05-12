8b0a4bba75c366470ebbe23997bbca71182bbdb1
Landing35 <87314063+Landing35@users.noreply.github.com>
2025-05-11 22:49:29 -0400

    Update mentors.js

--- data/mentors.js
+++ data/mentors.js
@@ -1,1 +1,26 @@
-
+export default [
+  {
+    id: 1,
+    name: 'John Smith',
+    title: 'Data Scientist',
+    profilePicture: 'https://source.unsplash.com/random/100x100/?johnsmith ',
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    title: 'Data Scientist',
+    profilePicture: 'https://source.unsplash.com/random/100x100/?janedoe ',
+  },
+  {
+    id: 3,
+    name: 'Mike Johnson',
+    title: 'Data Scientist',
+    profilePicture: 'https://source.unsplash.com/random/100x100/?mikejohnson ',
+  },
+  {
+    id: 4,
+    name: 'Emily Davis',
+    title: 'Data Scientist',
+    profilePicture: 'https://source.unsplash.com/random/100x100/?emilydavis ',
+  },
+];
